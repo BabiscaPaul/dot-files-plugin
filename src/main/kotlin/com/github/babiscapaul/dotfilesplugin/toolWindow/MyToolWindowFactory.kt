279da0eaf9b1ae9273981e653d76fb69c0ebc7a9
GitHub Action <action@github.com>
2023-11-08 17:46:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/babiscapaul/dotfilesplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/babiscapaul/dotfilesplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.babiscapaul.dotfilesplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.babiscapaul.dotfilesplugin.MyBundle
+import com.github.babiscapaul.dotfilesplugin.services.MyProjectService
 import javax.swing.JButton
 
 
